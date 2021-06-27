541fc834954efe5a45093259ccb7dfbbab1b4653
Tijo971 <84888671+Tijo971@users.noreply.github.com>
2021-06-27 09:42:06 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Info(){
+  return(
+      <div className="note">
+      <h1>JAVASCRIPT & AND REACAT.JS</h1>
+      <p>Study more about JAVASCRIPT & AND REACAT.JS </p>
+      </div>
+  );
+}
+export default Info;
